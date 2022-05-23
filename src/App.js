f99d8a194b4b622d9d3f42bcecbaaf7b2ac1c005
Shubham <shubhamsikarwar43@gmail.com>
2022-05-23 19:30:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
+import { useState } from 'react'
+import './App.css'
 
 function App() {
+  const [val, setVal] = useState({ task: '', isDone: false })
+  const [task, setTask] = useState([])
+
+  function addTask(e) {
+    e.preventDefault()
+    const newTask = [...task, { val }]
+    console.log(newTask)
+    setTask(newTask)
+    setVal('')
+  }
+
+  function taskCompleted(index) {
+    const newTask = [...task]
+    newTask[index].val.isDone = true
+    console.log(newTask)
+    setTask(newTask)
+  }
+
+  function delTask(index) {
+    const newTask = [...task]
+    newTask.splice(index, 1)
+    setTask(newTask)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <input
+        onChange={(e) => setVal({ task: e.target.value, isDone: false })}
+      />
+      <button onClick={addTask}>Add</button>
+      {task &&
+        task.map((task, index) => {
+          return (
+            <div key={index} className={task.val.isDone ? 'cdiv' : 'div'}>
+              <div>{task.val.task}</div>
+              <button onClick={() => taskCompleted(index)}>
+                {task.val.isDone ? 'Completed' : 'InComplete'}
+              </button>
+              <button onClick={() => delTask(index)}>Delete</button>
+            </div>
+          )
+        })}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
